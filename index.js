b369d94182bdf2e6ce803ecb051e39afa128e376
Lucas Soares <lucassfb@gmail.com>
2021-09-15 13:35:02 -0300

    envio principal

---
+++ index.js
@@ -0,0 +1,31 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const cors = require("cors");
+const express = require("express");
+const Conn = require("./models/conn/tarefa.conn");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+Conn(db_url, db_user, db_pass, db_data);
+
+const porta = 3001;
+
+const tarefa = require("./routers/tarefa.routes");
+app.use("/tarefa", tarefa);
+
+app.listen(process.env.PORT || porta, () => {
+  console.info(`O app está rodando em : http://localhost:${porta}/`);
+});
---
+++ routers/tarefa.routes.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const Tarefa = require("../models/tarefa.model");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  await Tarefa.find({})
+    .then((tarefa) => {
+      res.send(tarefa);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+router.get("/FindById/:id", async (req, res) => {
+  await Tarefa.findOne({ _id: req.params.id })
+    .then((tarefa) => {
+      res.send(tarefa);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+router.post("/add", async (req, res) => {
+  if (
+    !Tarefa ||
+    !Tarefa.titulo ||
+    !Tarefa.descrição ||
+    !Tarefa.prioridade ||
+    !Tarefa.status ||
+    !Tarefa.prazo
+  ) {
+    await Tarefa.create(req.body)
+      .then(() => {
+        res.status(200).send("Tarefa adicionada com sucesso");
+      })
+      .catch((err) => {
+        res.status(400).send("Algo errado. Tente novamente!");
+        console.error(err);
+      });
+  }
+});
+
+router.put("/update/:id", async (req, res) => {
+  if (
+    !Tarefa ||
+    !Tarefa.titulo ||
+    !Tarefa.descrição ||
+    !Tarefa.prioridade ||
+    !Tarefa.status ||
+    !Tarefa.prazo
+  ) {
+    await Tarefa.updateOne({ _id: req.params.id }, req.body)
+      .then(() => {
+        res.status(200).send("Tarefa atualizada com sucesso!");
+      })
+      .catch((err) => {
+        res.status(400).send("Algo errado. Tente novamente!");
+        console.error(err);
+      });
+  } else {
+    res.status(400).send("Algo errado. Tente novamente!");
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  await Tarefa.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).send("A tarefa foi apagada!");
+    })
+    .catch((err) => {
+      res.status(400).send("Algo errado. Tente novamente!");
+      console.error(err);
+    });
+});
+
+module.exports = router;
